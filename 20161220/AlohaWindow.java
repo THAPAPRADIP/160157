62667eb0f15a95db5fed60dcd24326b672d86065
THAPAPRADIP <pradip.pradip.thapa8@gmail.com>
2016-12-20 11:41:20 +0900

    AlohaWindow done.

--- 20161220/AlohaWindow.java
+++ 20161220/AlohaWindow.java
@@ -4,7 +4,19 @@ import javax.swing.*;
 public class AlohaWindow{
 	public static void main(String[]args){
 	JFrame frame=new JFrame();
-	frame.setSize(200,200);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setSize(500,200);
+	frame.setTitle("タパ");
+
+	frame.setLayout(new FlowLayout());
+	JLabel label=new JLabel("HELLO");
+	Container ctnr=frame.getContentPane();
+	ctnr.add(label);
+
+	JButton button=new JButton("CLICK");
+	ctnr.add(button);
+
 	frame.setVisible(true);
+
 	}
 	}
