7c1b50422ab7cc0c38a27483351023af4259a3b8
THAPAPRADIP <pradip.pradip.thapa8@gmail.com>
2016-12-22 10:09:57 +0900

    MIANFRAMEMAN

---
+++ 20161222/MainFrameMan.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainFrameMan{
+  	public static void main(String[] args){
+		FrameMan fm=new FrameMan();
+	}
+}
+	class FrameMan{
+	JFrame frame;
+	public FrameMan(){
+	System.out.println("aloha");
+	frame=new JFrame();
+	frame.setLocation(200,200);
+	frame.setSize(300,300);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+}
+}
